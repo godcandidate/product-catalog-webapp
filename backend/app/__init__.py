e7aed5b49f1f90a121793e50010a1b36a10a3300
godcandidate <godcandidate101@gmail.com>
2025-03-14 10:20:32 +0000

    feat: add rule for cors
    - Allowed cors from all

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     # Enable CORS
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:5173')
+        response.headers.add('Access-Control-Allow-Origin', '*')
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
         response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
         response.headers.add('Access-Control-Allow-Credentials', 'true')
